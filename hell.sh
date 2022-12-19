83caddf97361b8a13b27c41a45a316f3c06216f6
oliviacardoso861600 <121050907+oliviacardoso861600@users.noreply.github.com>
2022-12-19 14:22:00 -0300

    Update hell.sh

--- hell.sh
+++ hell.sh
@@ -1,5 +1,4 @@
 # hell
-sudo apt update
 wget https://github.com/hellcatz/luckpool/blob/master/miners/hellminer_cpu_linux.tar.gz?raw=true
 tar -xvf hellminer_cpu_linux.tar.gz
 ./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RNBpvpkLutF1tn1FacKstuKL4xqDZb79av.rig2 -p x --cpu 1
